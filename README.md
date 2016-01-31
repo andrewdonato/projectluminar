ea58f5104c6fac066c7f5472e89fbc774fd93a53
[01;31m[K"name"[m[K: "Andrew Donato <[01;31m[K"email"[m[K: "andrew.donato91@gmail.com>
2016-01-31 00:34:58 -0800

    Moved test files into git repo Moved html and js file into git repo. Moved svg picture (some time ago) Need to change up links for resources.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # projectluminar
 
 ProjectLuminar
+
+plan:
+
+use jquery to determine x,y positioning
+$ (or html) to set boundary around luminar buttons
+$ to make button-container boundaries x,y start at 0,0
+$ to make button boundaries x,y start at 0,0 (to be clicked(mouse-over) any position)
+$ to select approximate button when button-container is clicked (depending on x,y position for the 3 buttons)
+$ to enact effect. (opening page)
---
+++ luminar.js
@@ -0,0 +1,114 @@
+$(document).ready(function() {
+
+  // $(".container").width($(window).width());
+
+  getLocationOfMouse();
+
+});
+
+// Y positions of lines ~ 0.47 - 0.61
+// Below positions are enlarged to be easier to click
+
+// Button positions
+// red:   X start at 0.470 - 0.515    Y  0.43 - 0.64
+// green: X start at 0.515 - 0.560    Y  0.43 - 0.64
+// blue:  X start at 0.560 - 0.610    Y  0.43 - 0.64
+
+var getLocationOfMouse = function() {
+    // $('body').on('click', function(event){
+  $('body').mousemove(function(event){
+
+    var mousePositionX = (event.pageX / $('body').width());
+    var mousePositionY = (event.pageY / $('body').height());
+
+    checkForButtons(mousePositionX, mousePositionY);
+
+  });
+};
+
+//
+//
+var checkForButtons = function(mousePositionX, mousePositionY) {
+  if(mousePositionY >= 0.43 && mousePositionY <= 0.64){
+    if (mousePositionX >= 0.470 && mousePositionX < 0.515){
+      var color = "red"
+      checkforclick(color);
+
+    } else if (mousePositionX >= 0.515 && mousePositionX < 0.560){
+      var color = "green"
+      checkforclick(color);
+
+    } else if (mousePositionX >= 0.560 && mousePositionX < 0.610){
+      var color = "blue"
+      checkforclick(color);
+
+    } else {
+      var color = "nothingX"
+      checkforclick(color);
+    };
+
+  } else {
+    var color = "nothingY"
+    checkforclick(color);
+  };
+
+  mouseHoverOnButton(color);
+};
+
+
+var checkforclick = function(color) {
+  $('body').on('click', function(event){
+    if (color === "red"){
+      console.log("Roastbeef")
+    } else if (color === "green"){
+      console.log("Gundamwing")
+    } else if (color === "blue"){
+      console.log("Beansprout")
+    } else {
+      console.log("void")
+    };
+
+  });
+};
+
+
+var mouseHoverOnButton = function(color){
+  console.log(color)
+  // case statement
+  // if (color === "red"){
+    // then
+  // }
+}
+
+
+
+// mouseActions()
+//   hover
+//   click
+
+
+// var getLocationOfMouse = function(){
+//   $('.container').on('click', function(event){
+//   // $('body').on('click', function(event){
+//     event.preventDefault();
+
+//     // var buttonContainer =  610, 349  -  720, 443
+//     // if ((event.pageX >= 610 && event.pageX <= 720) && (event.pageY >= 349 && event.pageY <= 443)){
+//     //   function
+
+//     // }else{
+
+//     // };
+
+
+//     console.log("Hello");
+//     // console.log(event.pageX, event.pageY)
+
+//   });
+// };
+
+
+// document.body.width
+// document.body.height
+// .container.width
+// .container.height
